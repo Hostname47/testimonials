3c8e916159e08e5cef8a5a8c40d174763ee3f589
HOSTNAME47 <mouadstev1@gmail.com>
2022-08-10 02:17:50 +0100

    style testimonials component

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,24 @@
 import testimonials from "./testimonials.js";
 
-/**
- * Testimonials box will take the height of the longest testimonial
- */
-
 testimonials.forEach(testimonial => {
     let container = document.querySelector('#testimonials-box .container');
     let component = document.querySelector('.testimonial-component-skeleton').cloneNode(true);
 
     /** put testimonial data to component */
-    
+    let avatar = component.querySelector('.avatar');
+    avatar.src = testimonial.avatar;
+    avatar.alt = testimonial.name;
+    avatar.parentNode.href = testimonial.avatar;
+    component.querySelector('.name').textContent = testimonial.name;
+    component.querySelector('.designation').textContent = testimonial.designation;
+    component.querySelector('.message').textContent = testimonial.message;
 
-    component.classList.remove('none');
-    component.classList.remove('testimonial-component-skeleton');
+    component.classList.remove(...['testimonial-component-skeleton', 'none']);
     container.append(component);
 });
+
+/**
+ * Testimonials box needs to set height because the container inside of it is absolute positioned
+ * so we need to set the height explicitly
+ */
+document.querySelector('#testimonials-box').style.height = `${document.querySelector('#testimonials-box .container').offsetHeight}px`;
